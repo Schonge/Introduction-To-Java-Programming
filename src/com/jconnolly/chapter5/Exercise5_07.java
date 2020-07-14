8dcde903caf2167132b2e67466cf0125b9cbbf53
John Connolly <schonge10@hotmail.com>
2020-07-13 20:21:59 -0400

    Completed Chapter 5 Exercises 7

--- src/com/jconnolly/chapter5/Exercise5_07.java
+++ src/com/jconnolly/chapter5/Exercise5_07.java
@@ -29,13 +29,20 @@ public class Exercise5_07 {
 			System.out.print("Please enter yearly interest rate: ");
 			yearlyInterestRate = input.nextDouble();
 		}
-		double monthlyInterestRate = yearlyInterestRate / 12;
-		futureInvestmentValue(amount, monthlyInterestRate, YEARS);
+		double monthlyInterestRate = yearlyInterestRate / 1200;
+		System.out.println("The amount invested is: $" + amount);
+		System.out.println("Annual interest rate: " + yearlyInterestRate + "%");
+		System.out.println("Years\tFuture Value");
+		
+		for(int year = 1; year <= YEARS; year++) {
+			System.out.printf("%5d\t%12.2f", year, futureInvestmentValue(amount, monthlyInterestRate, year));
+			System.out.println();
+		}
+		
 	}
 	
 	public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years) {
-		double monies = 0;
-		return monies;
+		return (int)(investmentAmount * Math.pow((1 + monthlyInterestRate), (years * 12)) * 100) / 100.0;
 	}
 
 }
